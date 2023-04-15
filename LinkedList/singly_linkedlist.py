2133a7455dd36d9a159aeab7c593a3523b3f89d2
Sumit Maurya <sumitmaurya201.sm@gmail.com>
2023-04-15 21:48:24 +0530

    delete at index in singly linked list

--- LinkedList/singly_linkedlist.py
+++ LinkedList/singly_linkedlist.py
@@ -9,13 +9,14 @@ class SinglyLinkedList:
         self.head = None
         self.tail = None
 
+    # representation of single linked list
     def __str__(self):
         curr = self.head
         lst_repr = []
         while curr:
             lst_repr.append(str(curr.data))
             curr = curr.next
-        return "["+ ", ".join(lst_repr) + "]" if lst_repr != [] else "[]"
+        return "["+ "-> ".join(lst_repr) + "]" if lst_repr != [] else "[]"
     
 
     def append(self,data):
@@ -72,6 +73,36 @@ class SinglyLinkedList:
                 prev.next = curr.next
                 curr = None
 
+    def delete_at_index(self,index):
+        curr = self.head
+        if curr is None:return
+        # if index at 0 
+        if index == 0: 
+            if curr.next is not None:
+                self.head = curr.next
+                curr = None
+            else:
+                self.head = curr.next
+                self.tail = self.head
+                curr = None
+        pos = 0 
+        prev = None
+        while curr and pos != index:
+            pos += 1
+            prev = curr
+            curr = curr.next
+        
+        if curr is not None:
+            if curr.next is None:
+                prev.next = curr.next
+                curr = None
+                self.tail = prev
+            else:
+                prev.next = curr.next
+                curr = None
+
+
+
 
 
         
@@ -94,6 +125,11 @@ if __name__ == "__main__":
         l.delete(i)
     
     print("\n after deleting: ",l)
+    
+    print("before deleting at index: ",l)
+    for i in range(4,10):
+        l.delete_at_index(i)
+        print(f"after deleting at index : {i} ",l)
     
 
         
